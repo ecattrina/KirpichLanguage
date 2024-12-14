2ce58cb6e463b43b3b9d84411746661b05d32455
ecattrina <katy_060104@mail.ru>
2024-12-14 20:19:43 +0500

    Установка пакета

--- KirpichLanguage/Program.cs
+++ KirpichLanguage/Program.cs
@@ -1,10 +1,1 @@
-﻿namespace KirpichLanguage
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿
